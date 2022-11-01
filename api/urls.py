8c119cced28e58253a2099f347181366d0eb0ef2
Pr1marry <chief.virtus2016@yandex.ru>
2022-11-01 15:38:39 +0300

    static + view_get

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,9 @@
 from django.template.defaulttags import url
 from django.urls import path
 
-from .views import TableViewSet, AlbumViewSet, ArtistViewSet
+from .views import TableViewSet
 
 urlpatterns = [
     path('table_list', TableViewSet.as_view({'get': 'list'})),
-    path('album_list', AlbumViewSet.as_view({'get': 'list'})),
-    path('artist_list', ArtistViewSet.as_view({'get': 'list'})),
     # path('track_list/<int:pk>/', TrackViewSet.as_view({'get': 'get_list'})),
 ]
